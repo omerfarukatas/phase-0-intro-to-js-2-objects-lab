96ec96b9f421cda210c1b8f63041ed831bff8dca
Omer <omerfarukatas96@gmail.com>
2022-04-16 12:35:18 +0300

    one function left

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: "Omer",
+    street: "Gulyali",
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+    const newObj = {...object};
+    newObj[key] = value;
+    return newObj;
+}
+
+// YUKARIDAKININ UYGULANISI VE SONUCU:
+//      const newObj = updateEmployeeWithKeyAndValue(employee, "name", "Faruk")
+//      console.log(newObj);
+//      => { name: 'Faruk', street: 'Gulyali' }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key){
+    const newObj = {...object};
+    delete newObj[key];
+    return newObj;
+}
+
+
+
